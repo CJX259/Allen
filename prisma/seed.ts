8328a962f177e8c526fbe68c47772e05f0011c32
jessy <jessy@jessydeMacBook-Pro.local>
2022-02-17 13:41:31 +0800

    feat: db数据初始化

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,9 +9,15 @@ const db = new PrismaClient();
 async function main() {
   const alice = await db.user.create({
     data: {
-      name: 'jessy1',
-      email: '1049602251',
-      jobTitle: '学生',
+      name: 'jessy',
+      realName: '陈建希',
+      idCard: '440902200001230431',
+      address: '广东省茂名市茂南区官渡2路248号大院',
+      mail: '1049602251@qq.com',
+      liveData: 'xxx',
+      liveMaxAge: new Date(),
+      role: 'ADMIN',
+      phone: '19927574193',
     },
   });
   console.log({ alice });
