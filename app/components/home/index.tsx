10cbdf0bc907d0ff74f834053eb8f712b3579b45
jessy <jessy@jessydeMacBook-Pro.local>
2022-03-26 12:55:35 +0800

    refactor: 处理用户卡片样式问题

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -15,7 +15,7 @@ export default function HomeCmop() {
         <h1>直播间: </h1>
         <Spin spinning={transition.state !== 'idle'}>
           {/* 搜索结果展示区 */}
-          <div className="search-content">
+          <div className="live-content">
             {data?.map((item) => <UserCardItem curUser={curUser} jumpRoom={true} key={item.id} data={item} />)}
           </div>
         </Spin>
